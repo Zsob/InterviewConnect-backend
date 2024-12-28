d067b046b65838c9b80799e2705c614c7120998d
曾极涵 <747039543@qq.com>
2024-12-28 10:38:58 +0800

    add questionBankQuestion相关实体类

---
+++ src/main/java/com/xyz/interviewConnect/model/dto/questionBankQuestion/QuestionBankQuestionAddRequest.java
@@ -0,0 +1,28 @@
+package com.xyz.interviewConnect.model.dto.questionBankQuestion;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 创建题库题目关联请求
+ *
+ * @author <a href="https://github.com/Zsob">Zsob</a>
+ */
+@Data
+public class QuestionBankQuestionAddRequest implements Serializable {
+
+    /**
+     * 题库 id
+     */
+    private Long questionBankId;
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+
+    private static final long serialVersionUID = 1L;
+}
